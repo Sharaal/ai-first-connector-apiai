b706f923878e1cc50254b5ea20716789d5d15190
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-02-28 15:43:25 +0100

    ai-connector-apiai

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Dependency Status](https://david-dm.org/dragonprojects/ai-connector-apiai.svg)](https://david-dm.org/dragonprojects/ai-connector-apiai)
+[![devDependency Status](https://david-dm.org/dragonprojects/ai-connector-apiai/dev-status.svg)](https://david-dm.org/dragonprojects/ai-connector-apiai?type=dev)
---
+++ src/web.js
@@ -0,0 +1,22 @@
+require('dotenv').config({ silent: true });
+
+const app = require('express')();
+app.listen(process.env.PORT);
+
+const rp = require('request-promise').defaults({
+  json: true,
+  url: process.env.AI_WEBHOOK_URL,
+});
+
+app.post('/', require('body-parser').json(), async (req, res) => {
+  const apiaiRequest = req.body;
+  const aiRequest = {
+    intent: apiaiRequest.result.metadata.intentName,
+    params: apiaiRequest.result.parameters,
+  };
+  const aiResponse = await rp.post({ body: aiRequest });
+  const apiaiResponse = {
+    speech: aiResponse.say,
+  };
+  res.send(apiaiResponse);
+});
