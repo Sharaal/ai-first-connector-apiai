9213fee63dffbc570057a65200a72d261800d9b9
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-03-07 12:51:03 +0100

    Logging should be stringified

--- src/web.js
+++ src/web.js
@@ -13,19 +13,20 @@ app.post('/', require('body-parser').json(), async (req, res) => {
   const apiaiRequest = req.body;
   const id = _.get(apiaiRequest, 'id');
   console.log(`id: ${id}`);
-  console.log(`(${id}) apiaiRequest: ${apiaiRequest}`);
+  console.log(`(${id}) apiaiRequest: ${JSON.stringify(apiaiRequest)}`);
   const aiRequest = {
     id,
     name: _.get(apiaiRequest, 'result.metadata.intentName'),
     params: _.get(apiaiRequest, 'result.parameters'),
+    session: {},
   };
-  console.log(`(${id}) aiRequest: ${aiRequest}`);
+  console.log(`(${id}) aiRequest: ${JSON.stringify(aiRequest)}`);
   const aiResponse = await rp.post({ body: aiRequest });
-  console.log(`(${id}) aiResponse: ${aiResponse}`);
+  console.log(`(${id}) aiResponse: ${JSON.stringify(aiResponse)}`);
   const apiaiResponse = {};
   if (aiResponse.say) {
     apiaiResponse.speech = aiResponse.say;
   }
-  console.log(`(${id}) apiaiResponse: ${apiaiResponse}`);
+  console.log(`(${id}) apiaiResponse: ${JSON.stringify(apiaiResponse)}`);
   res.send(apiaiResponse);
 });
