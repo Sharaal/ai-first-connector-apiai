1d3b0fde4d548d5c92d7c2cf243e06dc500b8c0c
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-03-07 12:36:07 +0100

    Add id and a lot of logging

--- src/web.js
+++ src/web.js
@@ -11,14 +11,21 @@ const rp = require('request-promise').defaults({
 
 app.post('/', require('body-parser').json(), async (req, res) => {
   const apiaiRequest = req.body;
+  const id = _.get(apiaiRequest, 'id');
+  console.log(`id: ${id}`);
+  console.log(`(${id}) apiaiRequest: ${apiaiRequest}`);
   const aiRequest = {
+    id,
     name: _.get(apiaiRequest, 'result.metadata.intentName'),
     params: _.get(apiaiRequest, 'result.parameters'),
   };
+  console.log(`(${id}) aiRequest: ${aiRequest}`);
   const aiResponse = await rp.post({ body: aiRequest });
+  console.log(`(${id}) aiResponse: ${aiResponse}`);
   const apiaiResponse = {};
   if (aiResponse.say) {
     apiaiResponse.speech = aiResponse.say;
   }
+  console.log(`(${id}) apiaiResponse: ${apiaiResponse}`);
   res.send(apiaiResponse);
 });
