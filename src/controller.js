c5c7635ae2a5a662936364527ede58937a2100c0
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-03-14 13:05:05 +0100

    Use "dcontrollers" and "dexpress"

---
+++ src/controller.js
@@ -0,0 +1,38 @@
+const _ = require('lodash');
+
+module.exports = ({ rp }) =>
+  ['post', ['/', require('body-parser').json(), async (req, res) => {
+    const apiaiRequest = req.body;
+    const id = _.get(apiaiRequest, 'id');
+    console.log(`id: ${id}`);
+    console.log(`(${id}) apiaiRequest: ${JSON.stringify(apiaiRequest)}`);
+    const aiRequest = {
+      id,
+      locale: _.get(apiaiRequest, 'lang'),
+      name: _.get(apiaiRequest, 'result.metadata.intentName'),
+      params: _.get(apiaiRequest, 'result.parameters'),
+      session: (() => {
+        const context = _.get(apiaiRequest, 'result.contexts', [])
+          .filter(context => context.name === 'session')[0];
+        if (!context) {
+          return {};
+        }
+        return context.parameters;
+      })(),
+    };
+    console.log(`(${id}) aiRequest: ${JSON.stringify(aiRequest)}`);
+    const aiResponse = await rp.post({ body: aiRequest });
+    console.log(`(${id}) aiResponse: ${JSON.stringify(aiResponse)}`);
+    const apiaiResponse = {};
+    if (aiResponse.say) {
+      apiaiResponse.speech = aiResponse.say;
+    }
+    if (aiResponse.display) {
+      apiaiResponse.displayText = `${aiResponse.display.title}: ${aiResponse.display.text}`;
+    }
+    if (aiResponse.session) {
+      apiaiResponse.contextOut = [{ name: 'session', lifespan: 1, parameters: aiResponse.session }];
+    }
+    console.log(`(${id}) apiaiResponse: ${JSON.stringify(apiaiResponse)}`);
+    res.send(apiaiResponse);
+  }]];
--- src/web.js
+++ src/web.js
@@ -1,46 +1,7 @@
 require('dotenv').config({ silent: true });
-const _ = require('lodash');
 
-const app = require('express')();
-app.listen(process.env.PORT);
-
-const rp = require('request-promise').defaults({
-  json: true,
-  url: process.env.AI_WEBHOOK_URL,
-});
-
-app.post('/', require('body-parser').json(), async (req, res) => {
-  const apiaiRequest = req.body;
-  const id = _.get(apiaiRequest, 'id');
-  console.log(`id: ${id}`);
-  console.log(`(${id}) apiaiRequest: ${JSON.stringify(apiaiRequest)}`);
-  const aiRequest = {
-    id,
-    locale: _.get(apiaiRequest, 'lang'),
-    name: _.get(apiaiRequest, 'result.metadata.intentName'),
-    params: _.get(apiaiRequest, 'result.parameters'),
-    session: (() => {
-      const context = _.get(apiaiRequest, 'result.contexts', [])
-        .filter(context => context.name === 'session')[0];
-      if (!context) {
-        return {};
-      }
-      return context.parameters;
-    })(),
-  };
-  console.log(`(${id}) aiRequest: ${JSON.stringify(aiRequest)}`);
-  const aiResponse = await rp.post({ body: aiRequest });
-  console.log(`(${id}) aiResponse: ${JSON.stringify(aiResponse)}`);
-  const apiaiResponse = {};
-  if (aiResponse.say) {
-    apiaiResponse.speech = aiResponse.say;
-  }
-  if (aiResponse.display) {
-    apiaiResponse.displayText = `${aiResponse.display.title}: ${aiResponse.display.text}`;
-  }
-  if (aiResponse.session) {
-    apiaiResponse.contextOut = [{ name: 'session', lifespan: 1, parameters: aiResponse.session }];
-  }
-  console.log(`(${id}) apiaiResponse: ${JSON.stringify(apiaiResponse)}`);
-  res.send(apiaiResponse);
-});
+const { app } = require('dexpress');
+const rp = require('request-promise').defaults({ json: true, url: process.env.AI_WEBHOOK_URL });
+require('dcontrollers')(app, [
+  require('./controller')({ rp }),
+]);
