9496a24f5b96dadc31b7a51fd39f896f7d49e687
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-03-20 10:20:58 +0100

    Go align with other connectors

--- src/transformers/response.js
+++ src/transformers/response.js
@@ -3,10 +3,9 @@ const _ = require('lodash');
 module.exports = aiResponse => ({
   speech: _.get(aiResponse, 'say', ''),
   displayText: (() => {
-    if (!_.has(aiResponse, 'display')) {
-      return;
+    if (_.has(aiResponse, 'display')) {
+      return `${_.get(aiResponse, 'display.title', '')}: ${_.get(aiResponse, 'display.text', '')}`;
     }
-    return `${_.get(aiResponse, 'display.title', '')}: ${_.get(aiResponse, 'display.text', '')}`;
   })(),
   contextOut: [{ name: 'session', lifespan: 1, parameters: _.get(aiResponse, 'session', {}) }],
 });
