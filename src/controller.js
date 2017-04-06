26290636b6004c13c37af9a4cbdf41af58007477
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-04-06 15:15:23 +0200

    Don't log the secret all the time

--- src/controller.js
+++ src/controller.js
@@ -2,14 +2,13 @@ module.exports = ({ rp, secret }) =>
   ['post', ['/', require('body-parser').json(), async (req, res) => {
     const apiaiRequest = req.body;
     let aiRequest, aiResponse, apiaiResponse, error;
-    const headers = { secret };
     try {
       aiRequest = require('./transformers/request')(apiaiRequest);
-      aiResponse = await rp.post({ body: aiRequest, headers });
+      aiResponse = await rp.post({ body: aiRequest, headers: { secret } });
       apiaiResponse = require('./transformers/response')(aiResponse);
     } catch (e) {
       error = e.message;
     }
-    console.log(`(${aiRequest.id}): ${JSON.stringify({ apiaiRequest, aiRequest, aiResponse, apiaiResponse, error, headers })}`);
+    console.log(`(${aiRequest.id}): ${JSON.stringify({ apiaiRequest, aiRequest, aiResponse, apiaiResponse, error })}`);
     res.send(apiaiResponse);
   }]];
