dff9414bb2f1b865d030501c7e28a102ad1d241d
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-02 12:21:50 +0200

    Upgrade "dexpress"

--- src/web.js
+++ src/web.js
@@ -1,6 +1,6 @@
 require('dotenv').config({ silent: true });
 
-const { app } = require('dexpress');
+const app = require('dexpress')();
 const rp = require('request-promise').defaults({ json: true, url: process.env.AI_WEBHOOK_URL });
 require('dcontrollers')(app, [
   require('./controller')({ rp, secret: process.env.AI_WEBHOOK_SECRET }),
