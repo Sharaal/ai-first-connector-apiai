cea34cd3c0fc6559956f4e8f10f5d66e760d3e10
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-23 15:21:56 +0200

    prettier

--- src/web.js
+++ src/web.js
@@ -3,9 +3,7 @@ require('dotenv-safe').config();
 const app = require('express')();
 app.disable('x-powered-by');
 
-require('@dnode/middlewares')(app, [
-  require('body-parser').json(),
-]);
+require('@dnode/middlewares')(app, [require('body-parser').json()]);
 
 require('@dnode/controllers')(app, [
   require('./controller')({
